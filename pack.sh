200cca754fd1d4166fd7c21443637d1a90742021
Yatao Li <yatli@microsoft.com>
2020-04-16 00:07:38 +0800

    update linux pack script

--- pack.sh
+++ pack.sh
@@ -1,16 +1,24 @@
 # !/bin/bash
 plat="win10-x64 linux-x64 osx.10.11-x64"
 
+rm -rf ./bin
 rm -rf ./out
-mkdir -p ./out/server
+rm -rf ./publish
 
 npm install
 npm run compile
+npm pack
+
+mkdir -p ./publish
+mkdir -p ./bin
+mv *.tgz ./publish/
 
 for i in $plat
 do
     dotnet publish -f netcoreapp3.1 -c Release --self-contained \
-        -r $i src/FSharpLanguageServer -o ./out/server/$i
+        -r $i src/FSharpLanguageServer -o ./bin/$i
+    pushd ./bin/$i
+    zip ../../publish/coc-fsharp-$i.zip ./*
+    popd
 done
  
-npm pack
